fe8862238d69bb8150f52289927063be2d1d409e
Kawal Jain <kawaljain03@gmail.com>
2025-05-02 12:30:36 +0530

    adding url, handle env variable,

--- config/app.py
+++ config/app.py
@@ -6,4 +6,5 @@ load_dotenv()
 class AppSettings:
     def __init__(self):
         self.SERPER_API_KEY = os.getenv("SERPER_API_KEY")
+        self.SERPER_API_URL = "https://google.serper.dev/news"
    
--- main.py
+++ main.py
@@ -1,15 +1,56 @@
-from fastapi import FastAPI  
-from config.app import AppSettings;
+from fastapi import FastAPI,HTTPException,APIRouter
+from config.app import AppSettings
+import httpx
+from typing import Optional
+from fastapi.middleware.cors import CORSMiddleware
 
 appSettings = AppSettings()
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
+api_router = APIRouter(prefix="/api/v1")
 
 @app.get('/')
 def index():
     return {"message":"This is Welcome Test"}
 
-@app.get('/search/{user}')
-async def search(user:str):
+@api_router.get('/')
+def index():
+    return {"message":"This is Welcome Test"}
+
+@api_router.get('/news/search')
+async def search(name: Optional[str] = None):
+
+    if not name:
+        raise HTTPException(status_code=400, detail="Name is required")
+
+    url = appSettings.SERPER_API_URL
+    headers = {
+        "X-API-KEY": appSettings.SERPER_API_KEY,
+        "Content-Type": "application/json", 
+    }
+    payload ={
+        "q": name
+    }
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.post(url, headers=headers,json=payload)
+            response.raise_for_status()  
+            data = response.json()
+            return {"articles": data.get("news", [])} 
+        except httpx.HTTPStatusError as e:
+            # Log and other action done here. use for debug And Store logs in db
+            print(e.response)
+            raise HTTPException(status_code=e.response.status_code, detail=f"API Error: Something Went Wrong Try after some time")
+        except Exception as e:
+            # Log and other action done here. use for debug And Store logs in db
+            print(e)
+            raise HTTPException(status_code=500, detail="API Error: Something Went Wrong Try after some time")
     return {"message":f"This is Welcome {user}"}
     
+app.include_router(api_router)
